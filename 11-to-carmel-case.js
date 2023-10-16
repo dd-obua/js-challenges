17febb917eb3391d04d5f87701461f779fa33f2b
Obua Denis Daniel <dd.obua@gmail.com>
2023-10-16 16:07:06 +0300

    Select textarea and button

--- 11-to-carmel-case.js
+++ 11-to-carmel-case.js
@@ -2,3 +2,6 @@
 
 document.body.append(document.createElement('textarea'));
 document.body.append(document.createElement('button'));
+
+const textarea = document.querySelector('textarea');
+const convertBtn = document.querySelector('button');
