955d9b64ba7f6265169d8e66ffbdab098284aa27
Obua Denis Daniel <dd.obua@gmail.com>
2023-10-16 16:08:29 +0300

    Create convert case function

--- 11-to-carmel-case.js
+++ 11-to-carmel-case.js
@@ -6,3 +6,19 @@ document.body.append(document.createElement('button'));
 const textarea = document.querySelector('textarea');
 const convertBtn = document.querySelector('button');
 convertBtn.innerText = 'Convert';
+
+const covertCase = () => {
+  const content = textarea.value;
+  if (!content) return;
+  const vars = content.split('\n');
+
+  for (const [i, v] of vars.entries()) {
+    const newVar = v.trim().toLowerCase();
+    const [first, second] = newVar.split('_');
+    const finalVar = `${first}${second.replace(
+      second[0],
+      second[0].toUpperCase()
+    )}`;
+    console.log(`${finalVar.padEnd(20)} ${'✅'.repeat(i + 1)}`);
+  }
+};
